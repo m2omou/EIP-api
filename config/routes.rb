aa8a8a09fb7897e0e685f621e4a4ef4156044026
mourad sabour <mourad.sabour@gmail.com>
2014-02-05 15:45:37 -0800

    change restpassword root

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,9 @@ EIP::Application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"  
   get "settings" => "settings#index", :as => "settings"
+  get "resetpassword" => "password_resets#new", :as => "resetpassword"
+  
+  
   resources :sessions
 
   get "welcome/index"
