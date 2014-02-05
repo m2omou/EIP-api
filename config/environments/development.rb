3876aeaa4718a4a27990a191662089371e90b2d6
mourad sabour <mourad.sabour@gmail.com>
2014-02-05 15:02:19 -0800

    change mail host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ EIP::Application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   
-  config.action_mailer.default_url_options = { :host => "ks3364443.kimsufi.com" }
+  config.action_mailer.default_url_options = { :host => "neerbyy.com" }
   
   config.cache_classes = false
 
