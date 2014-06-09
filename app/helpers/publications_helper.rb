3ca930d38c26a2d19a5eaca1d0aaa4b053d895f5
mourad sabour <mourad.sabour@gmail.com>
2014-06-08 23:36:55 -0700

    check youtube links

--- app/helpers/publications_helper.rb
+++ app/helpers/publications_helper.rb
@@ -1,29 +1,42 @@
 module PublicationsHelper
 
-  def self.checkPublicationType(pub, publication_params)
-    @types = { :link => 0, :unknown => 0, :text => 1,
-               :image => 2, :youtube => 3, :file => 4 }
+  PUB_TYPES = { :link => 0,
+                :unknown => 0,
+                :text => 1,
+                :image => 2,
+                :youtube => 3,
+                :file => 4 }
 
+  def self.checkPublicationType(pub, publication_params)
     if (pub.file_url != nil)
-      pub[:type] = @types[:image]
+      pub[:type] = PUB_TYPES[:image]
       pub[:url] =  pub.file.url
       pub[:thumb_url] = pub.file.thumb.url
     elsif (publication_params[:link] != nil)
-      pub[:type] = @types[:link]
+      pub[:type] = checkMediaSources(publication_params[:link])
       pub[:url] = publication_params[:link]
     elsif (publication_params[:content] != nil)
-      pub[:type] = @types[:text]
+      pub[:type] = PUB_TYPES[:text]
       pub[:url] = nil
     else
-      pub[:type] = @types[:unknown]
+      pub[:type] = PUB_TYPES[:unknown]
       pub[:url] = nil
     end
     return pub
   end
 
+  # check media type such as youtube, dailymotion, ...
+  def self.checkMediaSources(link)
+    if !isAYoutubeVideo?(link).nil?
+      return PUB_TYPES[:youtube]
+    end
+    return PUB_TYPES[:link]
+  end
+
+  # regex youtube videos
   def self.isAYoutubeVideo?(url)
     youtube_regex = %r{^(http|https):\/\/(?:www\.)?youtube.com\/watch\?(?=[^?]*v=\w+)(?:[^\s?]+)?$}xi
-    (url =~ youtube_regex) == 0 ? 3 : 0
+    (url =~ youtube_regex)
  end
 
 end
