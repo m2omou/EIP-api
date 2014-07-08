52cde8156054069731700e1330704ffbae809707
mourad sabour <mourad.sabour@gmail.com>
2014-07-08 03:40:51 -0700

    edit schema db

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20140526213838) do
     t.datetime "updated_at"
   end
 
+  create_table "followed_places", force: true do |t|
+    t.integer  "user_id"
+    t.string   "place_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "messages", force: true do |t|
     t.text     "content"
     t.integer  "user_id"
