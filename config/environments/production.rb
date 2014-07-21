c96e137e770053bd1333716afc5c8ebeba26a07a
mourad sabour <mourad.sabour@gmail.com>
2014-07-21 03:44:27 -0700

    change the email host for the production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ EIP::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { :host => "http://neerbyy.com/" }
 
   config.action_mailer.delivery_method = :sendmail
 
