caabe865d08f52a45ecf7d54092ec79e90fb777a
sabour <sabour@MacBook-Pro-de-sabour.local>
2014-10-24 20:30:49 +0200

    remove access to backoffice template

--- app/controllers/backoffice_controller.rb
+++ app/controllers/backoffice_controller.rb
@@ -15,16 +15,12 @@ class BackofficeController < ApplicationController
   # ask for token access
   def restrict_access
     if  session[:user_id]
-      if !comment_params.nil?
-        comment_params[:user_id] = session[:user_id]
-      end
+
     else
       authenticate_or_request_with_http_token do |token, options|
         @user = User.where(:auth_token => token).first()
         if (@user)
-          if !comment_params.nil?
-            comment_params[:user_id] = @user.id
-          end
+
           return true
         end
         false
