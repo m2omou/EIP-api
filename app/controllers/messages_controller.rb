f633279c2bf4edd33938cea179446005d11f2b95
sabour <sabour@MacBook-Pro-de-sabour.local>
2014-12-06 03:13:35 +0100

    add sender_id to the data sent with the notif for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -107,7 +107,7 @@ class MessagesController < ApplicationController
             n.app = Rpush::Apns::App.find_by_name("ios_app")
             n.device_token = @recipient.device_token
             n.alert =  "#{@sender.username} vous a envoye un message."
-            n.data = { sender_id: @sender.id }
+            n.data = { sender_id: @sender.id, conversation_id: @message.conversation_id }
             n.save!
             # send
             Rpush.push
